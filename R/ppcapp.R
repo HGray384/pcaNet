b9d3f156eb95cbf7e645328f0c661449c977081f
Paul DW Kirk <paul.kirk@mrc-bsu.cam.ac.uk>
2018-04-17 15:08:06 +0100

    Updated ppcapp.R to return orthogonalised W matrix as res$W_orth, for compatability with pcaMethods.

--- R/ppcapp.R
+++ R/ppcapp.R
@@ -21,8 +21,24 @@ ppcapp <- function(myMat, nPcs=2, seed=NA, threshold=1e-5, maxIterations=1000, .
   
   ppcaOutput <- ppcaNet(myMat, N, D, C,  hidden, nMissing, nPcs, threshold, maxIterations=1000)
   
+  Worth <- orth(ppcaOutput$W)
+  evs   <- eigen(cov(myMat %*% C))
+  vals  <- evs[[1]]
+  vecs  <- evs[[2]]
+  Worth <- Worth %*% vecs
+  X     <- myMat %*% Worth
+  R2cum <- rep(NA, nPcs)
+  TSS   <- sum(myMat^2, na.rm = TRUE)
+  for (i in 1:ncol(Worth)) {
+    difference <- myMat - (X[, 1:i, drop = FALSE] %*% t(C[, 1:i, drop = FALSE]))
+    R2cum[i] <- 1 - (sum(difference^2, na.rm = TRUE)/TSS)
+  }
+  
+  
+  
   res <- list()
   res[["W"]]        <- ppcaOutput$W
+  res[["W_orth"]]   <- Worth
   res[["sigmaSq"]]  <- ppcaOutput$ss
   res[["C"]]        <- ppcaOutput$C
   res[["method"]]   <- "ppcaNet"
