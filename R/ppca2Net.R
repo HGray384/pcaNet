d2a0e7c637c4cffd2b144a43fa122851fb08e6cc
Harry Gray <hg384@cam.ac.uk>
2018-10-11 14:19:48 +0100

    updated network plotting and imports

--- R/ppca2Net.R
+++ R/ppca2Net.R
@@ -8,6 +8,14 @@
 #'   of the PPCA functions in this package.
 #' @param plot \code{logical} -- visualise the resulting network.
 #' @param verbose \code{logical} -- verbose intermediary output.
+#' @param vertex.size see \code{\link[igraph]{igraph.plotting}}
+#' @param edge.width see \code{\link[igraph]{igraph.plotting}}
+#' @param vertex.label.cex see \code{\link[igraph]{igraph.plotting}}
+#' @param vertex.color see \code{\link[igraph]{igraph.plotting}}
+#' @param vertex.label.color see \code{\link[igraph]{igraph.plotting}}
+#' @param edge.color see \code{\link[igraph]{igraph.plotting}}
+#' @param vertex.label.family see \code{\link[igraph]{igraph.plotting}}
+#' @param vertex.label see \code{\link[igraph]{igraph.plotting}}
 #' 
 #' @details Covariance estimation is done as a preliminary step for
 #'   this function. The function then inverts this matrix, which can
@@ -20,8 +28,8 @@
 #'   partial correlations that are not part of the 'null' component
 #'   are then selected as true edges of the network, effectively 
 #'   setting the null values to 0. The function then
-#'   visualises the resulting network using \code{\link[igraph]{plot.igraph}}. The
-#'   user can extract the \code{fdr.stats} element of this output to
+#'   visualises the resulting network using \code{\link[igraph]{plot.igraph}}.
+#'   The user can extract the \code{fdr.stats} element of this output to
 #'   view the full output of \code{\link[fdrtool:fdrtool]{fdrtool}},
 #'   from which the magnitude and significance of each partial 
 #'   correlation can be seen (and customised thresholding can be
@@ -40,11 +48,18 @@
 #' @export
 #'
 #' @examples
-ppca2Net <- function(ppcaOutput, plot=TRUE, verbose=TRUE){
+ppca2Net <- function(ppcaOutput, plot=TRUE, verbose=TRUE, vertex.size = 10, 
+                     edge.width = 2, vertex.label.cex = 0.4, 
+                     vertex.color = "cyan", vertex.label.color = "black", 
+                     edge.color = "pink", vertex.label.family= "Helvetica", 
+                     vertex.label = NULL){
+  
+  # extract the covariance matrix estimate
+  cov.mat <- ppcaOutput$Sigma
   
   # get variable names
-  if(!is.null(colnames(ppcaOutput$Sigma))){
-    labs <- colnames(ppcaOutput$Sigma)
+  if(!is.null(colnames(cov.mat))){
+    labs <- colnames(cov.mat)
   } else {
     labs <- NULL
     if(verbose){
@@ -87,20 +102,42 @@ ppca2Net <- function(ppcaOutput, plot=TRUE, verbose=TRUE){
   diag(net.mat) <- rep(1, nrow(pcor.mat))
   colnames(net.mat) <- labs
   rownames(net.mat) <- labs
-    
+  
   
   g <- igraph::graph_from_adjacency_matrix(adjmatrix = net.mat,
-                                      mode = "undirected",
-                                      weighted = TRUE,
-                                      diag = FALSE)
+                                           mode = "undirected",
+                                           weighted = TRUE,
+                                           diag = FALSE)
   g <- igraph::delete.vertices(g, igraph::degree(g)==0)
+  
+  #nEdges     <- gsize(g)
+  
+  
+  igraph::V(g)$color        <- vertex.color #define node colour
+  igraph::V(g)$frame.color  <- igraph::V(g)$color #define node colour
+  igraph::V(g)$size         <- vertex.size #define node colour
+  if(!is.null(vertex.label))
+  {
+    igraph::V(g)$label        <- vertex.label
+  }
+  igraph::E(g)$color        <- edge.color#edgeColors
+  igraph::E(g)$width        <- edge.width*(igraph::E(g)$weight/max(igraph::E(g)$weight))
+  igraph::V(g)$label.family <- vertex.label.family
+  igraph::V(g)$label.cex    <- vertex.label.cex
+  igraph::V(g)$label.color  <- vertex.label.color 
   
   # look to plot the graph
   if (plot && any(edge.stats$qval<edge.stats$param[1])){
     plot(g,
-         main = "Estimated network",
-         vertex.size = 10,
-         edge.width = 2)
+         #main = "Estimated network",
+         #vertex.size = vertex.size,
+         #edge.width = edge.width*(E(g)$weight/max(E(g)$weight)),
+         #vertex.label.family = "Helvetica",
+         #vertex.frame.color="red",
+         #edge.color = edgeColors,
+         #main = "Conditional dependence network",
+         layout=igraph::layout.fruchterman.reingold
+    )
   }
   
   # create output
