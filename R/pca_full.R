fec7ef4f9b5c20c8c4dd183c43e62ba6bcc23013
Harry Gray <hg384@cam.ac.uk>
2018-12-28 13:33:47 +0000

    added restarts to pca_full for bad estimation correction

--- R/pca_full.R
+++ R/pca_full.R
@@ -65,265 +65,293 @@ pca_full <- function(X, ncomp=NA, algorithm = "vb", maxiters = 1000,
   # X <- matrix(rnorm(20000), 100, 200)
   # X[1, 2] <- NaN
   
-  opts <- list(init='random',
-               maxiters=as.numeric(1000),
-               # bias=as.numeric(1),
-               # 'uniquesv'=0,
-               # 'autosave'=600,
-               # 'filename'='pca_f_autosave',
-               # 'minangle'=1e-8,
-               # 'algorithm'='vb',
-               niter_broadprior=as.numeric(100),
-               earlystop=as.numeric(0)
-               # 'rmsstop'=[ 100 1e-4 1e-3 ], # [] means no rms stop criteria
-               # 'cfstop'=[], # [] means no cost stop criteria
-               # 'verbose'=1,
-               # 'xprobe'=[],
-               # 'rotate2pca'=1,
-               # 'display'=0
-  )
+  savedX <- X
+  savedncomp <- ncomp
+  savedalgorithm <- algorithm
+  savedmaxiters <- maxiters
+  savedbias <- bias
+  savedrotate2pca = rotate2pca
+  savedloglike = loglike
+  savedverbose = verbose
+  myCondNum <- 1000
+  numOuterIts <- 1
   
-  if(algorithm == "vb")
+  while(myCondNum >= 1000 && numOuterIts < 5)
   {
-    use_prior = 1
-    use_postvar = 1
-  }
-  else if(algorithm == "map")
-  {
-    use_prior = 1
-    use_postvar = 0
+    X <- savedX
+    ncomp <- savedncomp
+    algorithm <- savedalgorithm
+    maxiters <- savedmaxiters
+    bias <- savedbias
+    rotate2pca <- savedrotate2pca
+    loglike <- savedloglike
+    verbose <- savedverbose
     
-  }
-  else if(algorithm == "ppca")
-  {
-    use_prior = 0
-    use_postvar = 0
-  }
-  else{
-    stop("Algorithm must be one of 'vb', 'map' or 'ppca'")  
-  }
-  
-  p <- nrow(X)
-  n <- ncol(X)
-  if (is.na(ncomp)){
-    ncomp <- min(n, p)-1
-  }
-  
-  # Missing values are marked as NaNs for consistency with MATLAB code,
-  # from which this was translated
-  if (any(is.na(X))){
-    X[which(is.na(X))] <- NaN
-  }
-  M = !is.nan(X)
-  
-  myMatsaved   <- X
-  X[X==0]      <- .Machine$double.eps
-  X[is.nan(X)] <- 0
-  
-  Nobs_i = rowSums(M)
-  
-  
-  notmiss <- which(X!=0, arr.ind = TRUE)
-  IX      <- notmiss[,1]
-  JX      <- notmiss[,2]
-  ndata   <- length(IX)
-  # clear data
-  rm(notmiss)
-  
-  # % Compute indices Isv: Sv{Isv(j)} gives Sv for j, j=1...n2
-  # these arguments are used to save memory when many Sv{j} are the same
-  nobscomb <- n # not currently used but might be in a future release
-  Isv      <- c()
-  obscombj <- c()
-  # end
-  
-  
-  ####################################
-  # parameter initialisation
-  
-  initialisedParms <- initParms(p, n, ncomp, verbose = verbose)
-  A   <- initialisedParms$A
-  S   <- initialisedParms$S
-  Mu  <- initialisedParms$Mu
-  V   <- initialisedParms$V
-  Av  <- initialisedParms$Av
-  Sv  <- initialisedParms$Sv
-  Muv <- initialisedParms$Muv
-  # 
-  # write.table( A, file = "A.csv", row.names = F, col.names = F, sep = ",")
-  # write.table( S, file = "S.csv", row.names = F, col.names = F, sep = ",")
-  # write.table( Mu, file = "Mu.csv", row.names = F, col.names = F, sep = ",")
-  # write.table( V, file = "V.csv", row.names = F, col.names = F, sep = ",")
-  # write.table( Av, file = "Av.csv", row.names = F, col.names = F, sep = ",")
-  # write.table( Sv, file = "Sv.csv", row.names = F, col.names = F, sep = ",")
-  # write.table( Muv, file = "Muv.csv", row.names = F, col.names = F, sep = ",")
-  
-  
-  ####################################
-  
-  Va  <- 1000*rep(1,ncomp)
-  Vmu <- 1000
-  
-  if (!use_prior)
-  {
-    Va  <- Va *Inf
-    Vmu <- Vmu *Inf 
-  }
-  
-  # the use_postvar options from the matlab code are
-  # executed in the pca_updates.cpp file linea 64-68
-  if (!bias){
-    # Muv is cleared in pca_updates.cpp line 72
-    # Muv = c()
-    Vmu = 0
-  }
-  
-  if (is.null(Mu)){
-    if (bias){
-      Mu <- rowSums(X) / Nobs_i
-    }else{
-      Mu = rep(0, p)
+    opts <- list(init='random',
+                 maxiters=as.numeric(1000),
+                 # bias=as.numeric(1),
+                 # 'uniquesv'=0,
+                 # 'autosave'=600,
+                 # 'filename'='pca_f_autosave',
+                 # 'minangle'=1e-8,
+                 # 'algorithm'='vb',
+                 niter_broadprior=as.numeric(100),
+                 earlystop=as.numeric(0)
+                 # 'rmsstop'=[ 100 1e-4 1e-3 ], # [] means no rms stop criteria
+                 # 'cfstop'=[], # [] means no cost stop criteria
+                 # 'verbose'=1,
+                 # 'xprobe'=[],
+                 # 'rotate2pca'=1,
+                 # 'display'=0
+    )
+    
+    if(algorithm == "vb")
+    {
+      use_prior = 1
+      use_postvar = 1
     }
-  }
-  
-  
-  
-  # data centering
-  X <- subtractMu(Mu, X, M, p, n, bias, verbose = verbose) 
-  ############################
-  # compute initial rms
-  ############################
-  
-  computedRMS <- compute_rms(X, A, S, M, ndata, verbose = verbose)
-  errMx       <- computedRMS$errMx
-  rms         <- computedRMS$rms
-  
-  ############################
-  # initial hyperprior parameter values
-  ############################
-  
-  hpVa <- 0.001
-  hpVb <- 0.001
-  hpV  <- 0.001
-  
-  #########################
-  # CALL C++ FUNCTION
-  #########################
-  if (is.null(Isv)){Isv <- rep(0, 2)}
-  IX <- IX -1 # C++ indexing
-  JX <- JX -1 # C++ indexing
-  if(verbose){
-    verbose <- 1 # C++ true
-  } else {
-    verbose <- 0 # C++ false
-  }
-  if(bias){
-    bias <- 1 # C++ true
-  } else {
-    bias <- 0 # C++ false
-  }
-  if(rotate2pca){
-    rotate2pca <- 1 # C++ true
-  } else {
-    rotate2pca <- 0 # C++ false
-  }
-  ppcaOutput <- pca_updates(X=X, V=V, A=A, Va=Va, Av = Av, S = S, Sv = Sv, 
-                            Mu = Mu, Muv = Muv, Vmu = Vmu,
-                            hpVa = hpVa, hpVb = hpVb, hpV = hpV, ndata = ndata, Nobs_i = Nobs_i,
-                            Isv = Isv, M = M, IX = IX, JX = JX, rms = rms, errMx = errMx, 
-                            bias = bias, rotate2pca = rotate2pca, niter_broadprior = opts$niter_broadprior, 
-                            use_prior = use_prior, use_postvar = use_postvar,
-                            maxiters = maxiters, verbose = verbose)
-  
-  #########################
-  # manage output
-  #########################
-  
-  nPcs <- ncomp
-  if(ppcaOutput$numIter == maxiters)
-  {
-    print('Maximum number of iterations reached')
-  }
-  
-  bias <- as.logical(bias)
-  
-  # R2 computation
-  R2cum      <- rep(NA, nPcs)
-  TSS        <- sum(myMatsaved^2, na.rm = TRUE)
-  
-  for (i in 1:ncomp) {
-    difference <- t(X) - (ppcaOutput$scores[, 1:i, drop = FALSE] %*% t(ppcaOutput$W[, 1:i, drop = FALSE]))
-    R2cum[i] <- 1 - (sum(difference^2, na.rm = TRUE)/TSS)
-  }
-  
-  
-  pcaMethodsRes           <- new("pcaRes")
-  pcaMethodsRes@scores    <- ppcaOutput$scores 
-  pcaMethodsRes@loadings  <- ppcaOutput$W
-  pcaMethodsRes@R2cum     <- R2cum
-  pcaMethodsRes@method    <- algorithm
-  pcaMethodsRes@missing   <- is.na(myMatsaved)
-  pcaMethodsRes@nPcs <- ncomp # do we need to edit this?
-  pcaMethodsRes@nObs <- ncol(myMatsaved)
-  pcaMethodsRes@nVar <- nrow(myMatsaved)
-  pcaMethodsRes@sDev <- apply(pcaMethodsRes@scores, 2, sd)
-  pcaMethodsRes@center <- as.numeric(ppcaOutput$m)
-  pcaMethodsRes@centered <- bias
-  pcaMethodsRes@scale <- rep(1, nrow(myMatsaved))
-  pcaMethodsRes@scaled <- "none"
-  pcaMethodsRes@R2 <- pcaMethodsRes@R2cum[1]
-  if (length(pcaMethodsRes@R2cum) > 1) {
-    pcaMethodsRes@R2 <- c(pcaMethodsRes@R2,
-                              diff(pcaMethodsRes@R2cum))
-  }
-  
-  completeObs <- myMatsaved
-  if(any(!M)){
-    recData <- tcrossprod(pcaMethodsRes@scores[, 1:nPcs, drop = FALSE],
-                          pcaMethodsRes@loadings[, 1:nPcs, drop = FALSE])
-    # recData <- sweep(recData, 2, sc, "*")
-    recData <- sweep(recData, 2, ppcaOutput$m, "+")
-    completeObs[is.na(myMatsaved)] <- recData[is.na(myMatsaved)]
-  }
-  pcaMethodsRes@completeObs <- completeObs
-  
-  # create hinton diagram
-  if(verbose){
-    plotrix::color2D.matplot(ppcaOutput$W,
-                             extremes=c("black","white"),
-                             main="Hinton diagram of loadings",
-                             Hinton=TRUE)
-  }
-  # back to R boolean
-  if(verbose==1){
-    verbose <- TRUE # C++ true
-  } else {
-    verbose <- FALSE # C++ false
-  }
-  
-  if (loglike){
-    # compute log-likelihood scores
-    loglikeobs <- compute_loglikeobs(myMatsaved, ppcaOutput$C, ppcaOutput$m,
-                                     verbose)
+    else if(algorithm == "map")
+    {
+      use_prior = 1
+      use_postvar = 0
+      
+    }
+    else if(algorithm == "ppca")
+    {
+      use_prior = 0
+      use_postvar = 0
+    }
+    else{
+      stop("Algorithm must be one of 'vb', 'map' or 'ppca'")  
+    }
     
-    loglikeimp <- compute_loglikeimp(myMatsaved, ppcaOutput$W, t(ppcaOutput$scores),
-                                     ppcaOutput$C, ppcaOutput$m, verbose)
+    p <- nrow(X)
+    n <- ncol(X)
+    if (is.na(ncomp)){
+      ncomp <- min(n, p)-1
+    }
+    
+    # Missing values are marked as NaNs for consistency with MATLAB code,
+    # from which this was translated
+    if (any(is.na(X))){
+      X[which(is.na(X))] <- NaN
+    }
+    M = !is.nan(X)
+    
+    myMatsaved   <- X
+    X[X==0]      <- .Machine$double.eps
+    X[is.nan(X)] <- 0
+    
+    Nobs_i = rowSums(M)
+    
+    
+    notmiss <- which(X!=0, arr.ind = TRUE)
+    IX      <- notmiss[,1]
+    JX      <- notmiss[,2]
+    ndata   <- length(IX)
+    # clear data
+    rm(notmiss)
+    
+    # % Compute indices Isv: Sv{Isv(j)} gives Sv for j, j=1...n2
+    # these arguments are used to save memory when many Sv{j} are the same
+    nobscomb <- n # not currently used but might be in a future release
+    Isv      <- c()
+    obscombj <- c()
+    # end
+    
+    
+    ####################################
+    # parameter initialisation
+    
+    initialisedParms <- initParms(p, n, ncomp, verbose = verbose)
+    A   <- initialisedParms$A
+    S   <- initialisedParms$S
+    Mu  <- initialisedParms$Mu
+    V   <- initialisedParms$V
+    Av  <- initialisedParms$Av
+    Sv  <- initialisedParms$Sv
+    Muv <- initialisedParms$Muv
+    # 
+    # write.table( A, file = "A.csv", row.names = F, col.names = F, sep = ",")
+    # write.table( S, file = "S.csv", row.names = F, col.names = F, sep = ",")
+    # write.table( Mu, file = "Mu.csv", row.names = F, col.names = F, sep = ",")
+    # write.table( V, file = "V.csv", row.names = F, col.names = F, sep = ",")
+    # write.table( Av, file = "Av.csv", row.names = F, col.names = F, sep = ",")
+    # write.table( Sv, file = "Sv.csv", row.names = F, col.names = F, sep = ",")
+    # write.table( Muv, file = "Muv.csv", row.names = F, col.names = F, sep = ",")
+    
+    
+    ####################################
+    
+    Va  <- 1000*rep(1,ncomp)
+    Vmu <- 1000
+    
+    if (!use_prior)
+    {
+      Va  <- Va *Inf
+      Vmu <- Vmu *Inf 
+    }
+    
+    # the use_postvar options from the matlab code are
+    # executed in the pca_updates.cpp file linea 64-68
+    if (!bias){
+      # Muv is cleared in pca_updates.cpp line 72
+      # Muv = c()
+      Vmu = 0
+    }
+    
+    if (is.null(Mu)){
+      if (bias){
+        Mu <- rowSums(X) / Nobs_i
+      }else{
+        Mu = rep(0, p)
+      }
+    }
+    
+    
+    
+    # data centering
+    X <- subtractMu(Mu, X, M, p, n, bias, verbose = verbose) 
+    ############################
+    # compute initial rms
+    ############################
+    
+    computedRMS <- compute_rms(X, A, S, M, ndata, verbose = verbose)
+    errMx       <- computedRMS$errMx
+    rms         <- computedRMS$rms
+    
+    ############################
+    # initial hyperprior parameter values
+    ############################
+    
+    hpVa <- 0.001
+    hpVb <- 0.001
+    hpV  <- 0.001
+    
+    #########################
+    # CALL C++ FUNCTION
+    #########################
+    if (is.null(Isv)){Isv <- rep(0, 2)}
+    IX <- IX -1 # C++ indexing
+    JX <- JX -1 # C++ indexing
+    if(verbose){
+      verbose <- 1 # C++ true
+    } else {
+      verbose <- 0 # C++ false
+    }
+    if(bias){
+      bias <- 1 # C++ true
+    } else {
+      bias <- 0 # C++ false
+    }
+    if(rotate2pca){
+      rotate2pca <- 1 # C++ true
+    } else {
+      rotate2pca <- 0 # C++ false
+    }
+    ppcaOutput <- pca_updates(X=X, V=V, A=A, Va=Va, Av = Av, S = S, Sv = Sv, 
+                              Mu = Mu, Muv = Muv, Vmu = Vmu,
+                              hpVa = hpVa, hpVb = hpVb, hpV = hpV, ndata = ndata, Nobs_i = Nobs_i,
+                              Isv = Isv, M = M, IX = IX, JX = JX, rms = rms, errMx = errMx, 
+                              bias = bias, rotate2pca = rotate2pca, niter_broadprior = opts$niter_broadprior, 
+                              use_prior = use_prior, use_postvar = use_postvar,
+                              maxiters = maxiters, verbose = verbose)
+    
+    #########################
+    # manage output
+    #########################
+    
+    nPcs <- ncomp
+    if(ppcaOutput$numIter == maxiters)
+    {
+      print('Maximum number of iterations reached')
+    }
+    
+    bias <- as.logical(bias)
+    
+    # R2 computation
+    R2cum      <- rep(NA, nPcs)
+    TSS        <- sum(myMatsaved^2, na.rm = TRUE)
+    
+    for (i in 1:ncomp) {
+      difference <- t(X) - (ppcaOutput$scores[, 1:i, drop = FALSE] %*% t(ppcaOutput$W[, 1:i, drop = FALSE]))
+      R2cum[i] <- 1 - (sum(difference^2, na.rm = TRUE)/TSS)
+    }
+    
+    
+    pcaMethodsRes           <- new("pcaRes")
+    pcaMethodsRes@scores    <- ppcaOutput$scores 
+    pcaMethodsRes@loadings  <- ppcaOutput$W
+    pcaMethodsRes@R2cum     <- R2cum
+    pcaMethodsRes@method    <- algorithm
+    pcaMethodsRes@missing   <- is.na(myMatsaved)
+    pcaMethodsRes@nPcs <- ncomp # do we need to edit this?
+    pcaMethodsRes@nObs <- ncol(myMatsaved)
+    pcaMethodsRes@nVar <- nrow(myMatsaved)
+    pcaMethodsRes@sDev <- apply(pcaMethodsRes@scores, 2, sd)
+    pcaMethodsRes@center <- as.numeric(ppcaOutput$m)
+    pcaMethodsRes@centered <- bias
+    pcaMethodsRes@scale <- rep(1, nrow(myMatsaved))
+    pcaMethodsRes@scaled <- "none"
+    pcaMethodsRes@R2 <- pcaMethodsRes@R2cum[1]
+    if (length(pcaMethodsRes@R2cum) > 1) {
+      pcaMethodsRes@R2 <- c(pcaMethodsRes@R2,
+                            diff(pcaMethodsRes@R2cum))
+    }
+    
+    completeObs <- myMatsaved
+    if(any(!M)){
+      recData <- tcrossprod(pcaMethodsRes@scores[, 1:nPcs, drop = FALSE],
+                            pcaMethodsRes@loadings[, 1:nPcs, drop = FALSE])
+      # recData <- sweep(recData, 2, sc, "*")
+      recData <- sweep(recData, 2, ppcaOutput$m, "+")
+      completeObs[is.na(myMatsaved)] <- recData[is.na(myMatsaved)]
+    }
+    pcaMethodsRes@completeObs <- completeObs
+    
+    # create hinton diagram
+    if(verbose){
+      plotrix::color2D.matplot(ppcaOutput$W,
+                               extremes=c("black","white"),
+                               main="Hinton diagram of loadings",
+                               Hinton=TRUE)
+    }
+    # back to R boolean
+    if(verbose==1){
+      verbose <- TRUE # C++ true
+    } else {
+      verbose <- FALSE # C++ false
+    }
+    
+    if (loglike){
+      # compute log-likelihood scores
+      loglikeobs <- compute_loglikeobs(myMatsaved, ppcaOutput$C, ppcaOutput$m,
+                                       verbose)
+      
+      loglikeimp <- compute_loglikeimp(myMatsaved, ppcaOutput$W, t(ppcaOutput$scores),
+                                       ppcaOutput$C, ppcaOutput$m, verbose)
+    }
+    
+    # Return standard ppcaNet output:
+    
+    output <- list()
+    output[["W"]]              <- ppcaOutput$W
+    output[["sigmaSq"]]        <- ppcaOutput$ss
+    output[["Sigma"]]          <- ppcaOutput$C
+    output[["m"]]              <- ppcaOutput$m
+    if (loglike){
+      output[["logLikeObs"]] <- loglikeobs
+      output[["logLikeImp"]] <- loglikeimp
+    }
+    output[["numIter"]]  <- ppcaOutput$numIter
+    output[["pcaMethodsRes"]]  <- pcaMethodsRes
+    
+    myCondNum   <- kappa(ppcaOutput$C, exact = TRUE)
+    numOuterIts <- numOuterIts + 1
   }
-  
-  # Return standard ppcaNet output:
-  
-  output <- list()
-  output[["W"]]              <- ppcaOutput$W
-  output[["sigmaSq"]]        <- ppcaOutput$ss
-  output[["Sigma"]]          <- ppcaOutput$C
-  output[["m"]]              <- ppcaOutput$m
-  if (loglike){
-    output[["logLikeObs"]] <- loglikeobs
-    output[["logLikeImp"]] <- loglikeimp
+  if (numOuterIts==6){
+    warning("PPCA did not find a good solution after 5 runs.")
   }
-  output[["numIter"]]  <- ppcaOutput$numIter
-  output[["pcaMethodsRes"]]  <- pcaMethodsRes
-  
   return(output)
 }
 
